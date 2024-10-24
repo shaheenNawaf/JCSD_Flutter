dd36f6ad6b669a1971fcfd795760e09c35b7dd82
Michael Guevara <mebguevara@addu.edu.ph>
2024-10-24 22:09:25 +0800

    top homepage

--- lib/view/homeView.dart
+++ lib/view/homeView.dart
@@ -6,9 +6,58 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Navbar(),
-      body: Center(
-        child: Text('Welcome to Home View!'),
-      ),
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background.png'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.6),
+                  BlendMode.darken,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/logo_white.png',
+                  height: 300,
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Araw-araw kalidad. All-day quality services to your hard-earned investments.',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Book Now'),
+                  style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 0, 174, 239),
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+                  textStyle: const TextStyle(fontSize: 40),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
     );
   }
 }
